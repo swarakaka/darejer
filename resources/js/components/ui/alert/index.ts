f12752793416ffd2c8654f82bd22103f8fade7dc
swarakaka <swara09@gmail.com>
2026-05-01 13:28:35 +0200

    refactor(ui): Adjust dialog and alert component styles
    The changes involve updating CSS class names in DialogContent.vue and index.ts to use 'start' and 'end' for directional positioning, likely for better RTL support. This is a minor refactoring of existing UI components.

--- resources/js/components/ui/alert/index.ts
+++ resources/js/components/ui/alert/index.ts
@@ -6,14 +6,14 @@ export { default as AlertDescription } from "./AlertDescription.vue"
 export { default as AlertTitle } from "./AlertTitle.vue"
 
 export const alertVariants = cva(
-  "relative w-full rounded-[2px] border-l-[3px] border-y border-r p-3 text-[13px] [&>svg~*]:ps-7 [&>svg+div]:translate-y-[-2px] [&>svg]:absolute [&>svg]:start-3 [&>svg]:top-3.5 [&>svg]:size-4",
+  "relative w-full rounded-[2px] border-s-[3px] border-y border-e p-3 text-[13px] [&>svg~*]:ps-7 [&>svg+div]:translate-y-[-2px] [&>svg]:absolute [&>svg]:start-3 [&>svg]:top-3.5 [&>svg]:size-4",
   {
     variants: {
       variant: {
         default:
-          "bg-brand-50 border-y-brand-100 border-r-brand-100 border-l-brand-500 text-ink-800 [&>svg]:text-brand-600",
+          "bg-brand-50 border-y-brand-100 border-e-brand-100 border-s-brand-500 text-ink-800 [&>svg]:text-brand-600",
         destructive:
-          "bg-danger-50 border-y-danger-100 border-r-danger-100 border-l-danger-500 text-ink-800 [&>svg]:text-danger-600",
+          "bg-danger-50 border-y-danger-100 border-e-danger-100 border-s-danger-500 text-ink-800 [&>svg]:text-danger-600",
       },
     },
     defaultVariants: {
